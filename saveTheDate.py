1af600e973e2c21269202cfb4015b8e4b2559bbc
Alessandro Piconi <59959880+invernomuto-git@users.noreply.github.com>
2024-11-25 17:00:06 +0100

    Update saveTheDate.py

--- saveTheDate.py
+++ saveTheDate.py
@@ -3,6 +3,12 @@ from datetime import datetime
 # Ottieni la data e l'ora correnti
 now = datetime.now()
 
-# Scrivi la data e l'ora in un file
+# Formatta la data e l'ora
+formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
+
+# Scrivi la data e l'ora in un file su una nuova riga
 with open('dataora.txt', 'a') as file:
-    file.write(now.strftime("%Y-%m-%d %H:%M:%S"))
+    file.write(formatted_time + '\n')  # Aggiungi una nuova riga
+
+# Stampa la data e l'ora nel terminale
+print(f"Data e ora salvate: {formatted_time}")
